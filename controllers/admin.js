2a5483dca64357cb298ac9074a3991a34c51d62e
ranasingh99 <sanketverma852@gmail.com>
2024-03-09 19:20:22 +0530

    addedd delete and edit

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+  
   });
 };
 
@@ -20,6 +18,21 @@ exports.postAddProduct = (req, res, next) => {
   res.redirect('/');
 };
 
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if(!editMode){
+       return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+    pageTitle: 'Edit Product',
+    path: '/admin/edit-product',
+    editing: editMode
+  });
+};
+
+exports.postDeleteProduct = (req,res,next)=>{
+  const prodId = req.body.productId;
+}
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('admin/products', {
